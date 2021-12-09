5b85e9a6e7cb5fcf0ef497f133505289f99a18bb
DeniseIra02 <irajubilo@gmail.com>
2021-12-09 20:36:20 +0800

    Program1 introduction using print statement

---
+++ Prog1.py
@@ -0,0 +1,7 @@
+print("-----------------------------------------------------------")
+print("Welcome to 3numLOTTERY! \n Come Play and Win!")
+print("-----------------------------------------------------------")
+print("Note! Make sure that your input digit are from 0-9 only.")
+print("-----------------------------------------------------------")
+
+import random
