2e476129f49f19be2e592887c283055b40b78e57
DeniseIra02 <irajubilo@gmail.com>
2021-12-10 15:08:42 +0800

    Program2 introduction and assigned variables for the random number.

---
+++ Prog2.py
@@ -0,0 +1,9 @@
+print("-----------------------------------------------------------")
+print("Welcome to U Guess the Num \n Come and Play!")
+print("-----------------------------------------------------------")
+print("Note! Make sure that your answer is a number.")
+print("-----------------------------------------------------------")
+
+import random
+
+the_num = random.randint(0,100)
